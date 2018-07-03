f33a73f0e654c0b5d829730d1f83fd19c3d3ca26
github username <github email address>
2018-07-03 15:01:36 -0400

    first commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+state = {
+    term: ''
+  }
+
+
+render() {
+   return (
+     <div>
+
+     </div>
+   );
+  }
+
+
+
+
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDm29u16R2js2wWq4fNMdFZ18dbLYYF_qA';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create new compononent
+// Should produce some html
+const App = () => {
+  return <div><SearchBar /></div>;
+}
+// Take this components generated HTML and put into DOM
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
